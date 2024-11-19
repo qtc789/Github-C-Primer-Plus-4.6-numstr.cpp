03044886b49e08320a786e4a8bf790ab329bd535
qtc789 <qiantongci@126.com>
2024-11-19 22:08:54 +0800

    Add files via upload

---
+++ 4.6 numstr.cpp/4.6 numstr.cpp/4.6 numstr.cpp.cpp
@@ -0,0 +1,20 @@
+﻿// 4.6 numstr.cpp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "what year was your house built?\n";
+    int year;
+    cin >> year;
+    cin.get();
+    cout << "what is its street address?\n";
+    char address[80];
+    cin. getline(address, 80);
+    cout << "year built;    " << year << endl;
+    cout << "address;    " << address << endl;
+
+    
+}
